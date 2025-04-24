472ca5c439104442a22d27cb39adb4244e602fbd
Vinniellf <a20213736@pucp.edu.pe>
2025-04-24 02:02:20 -0500

    activity adminhotel

---
+++ app/src/main/java/com/example/riderest/adminhotel/AdminHotelActivity.java
@@ -0,0 +1,27 @@
+package com.example.riderest.adminhotel;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.riderest.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AdminHotelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminhotel);
+
+
+    }
+}
